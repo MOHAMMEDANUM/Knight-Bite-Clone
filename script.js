1c32ebae21e9df763e39ecde2a144645fd6f0f51
Mohammed Anum <160215305+MOHAMMEDANUM@users.noreply.github.com>
2025-05-21 22:47:40 +0530

    (2nd)Second javascript Added succesfully for the menu's inner burger card closeup pop

--- script.js
+++ script.js
@@ -1,6 +1,58 @@
 
 // Menu Section 
- let popup = ()=>{
-    let card = document.querySelector("#plus")
-    card.style.display="flex"
+ let popup1 = ()=>{
+    let card1 = document.querySelector("#plus1")
+    card1.style.display="flex"
+ }
+ let closeup1 = ()=>{
+    let close1 = document.querySelector("#plus1")
+    close1.style.display="none"
+ }
+
+
+ let popup2 = ()=>{
+    let card2 = document.querySelector("#plus2")
+    card2.style.display="flex"
+ }
+ let closeup2 = ()=>{
+    let close2 = document.querySelector("#plus2")
+    close2.style.display="none"
+ }
+
+
+ let popup3 = ()=>{
+    let card3 = document.querySelector("#plus3")
+    card3.style.display="flex"
+ }
+ let closeup3 = ()=>{
+    let close3 = document.querySelector("#plus3")
+    close3.style.display="none"
+ }
+
+ let popup4 = ()=>{
+    let card4 = document.querySelector("#plus4")
+    card4.style.display="flex"
+ }
+ let closeup4 = ()=>{
+    let close4 = document.querySelector("#plus4")
+    close4.style.display="none"
+ }
+
+
+ let popup5 = ()=>{
+    let card5 = document.querySelector("#plus5")
+    card3.style.display="flex"
+ }
+ let closeup5 = ()=>{
+    let close5 = document.querySelector("#plus5")
+    close5.style.display="none"
+ }
+
+ let popup6 = ()=>{
+    let card6 = document.querySelector("#plus6")
+    card6.style.display="flex"
+ }
+ let closeup6 = ()=>{
+    let close6 = document.querySelector("#plus6")
+    close6.style.display="none"
  }
